1a141eb7be597ad839b9b6a6ab872163fab725b8
Picasso Houessou <houessoupicasso@yahoo.fr>
2024-05-24 13:20:10 +0200

    Search filter for ModuleType and ModuleStatus

--- assets/admin/layouts/Footer.tsx
+++ assets/admin/layouts/Footer.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {useTranslation} from "react-i18next";
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 export default function Footer() {
-    const {t} = useTranslation();
+    const { t } = useTranslation();
     return (
         <div className="main-footer">
             <span>&copy; 2024. Picasso Houessou. All Rights Reserved.</span>
             <span>
-                {t("Créé par :")}{' '}
+                {t('Créé par :')}{' '}
                 <Link to="https://picassohouessou.com" target="_blank">
                     Picasso Houessou
                 </Link>
