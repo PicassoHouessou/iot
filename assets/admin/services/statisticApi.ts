a0f9dd23d1ef0632888dc984890493a844eb0fc4
Picasso Houessou <houessoupicasso@yahoo.fr>
2024-05-24 10:33:46 +0200

    Bugs fixing: charts don't reload when we simulate

--- assets/admin/services/statisticApi.ts
+++ assets/admin/services/statisticApi.ts
@@ -1,18 +1,18 @@
-import { Statistic } from '../models';
-import { adminModuleApi } from './adminModuleApi';
-import { ApiRoutesWithoutPrefix } from '@Admin/constants';
+import {Statistic} from '../models';
+import {adminModuleApi} from './adminModuleApi';
+import {ApiRoutesWithoutPrefix} from '@Admin/constants';
 
 export const statisticApi = adminModuleApi.injectEndpoints({
     endpoints: (builder) => ({
         statistics: builder.query<Statistic[], void>({
             query: () => ApiRoutesWithoutPrefix.STATISTICS,
-            providesTags: ['Log'],
+            providesTags: ['Module'],
         }),
         statistic: builder.query<Statistic, string>({
             query: (id) => `${ApiRoutesWithoutPrefix.STATISTICS}/${id}`,
-            providesTags: ['Log'],
+            providesTags: ['Module'],
         }),
     }),
 });
 
-export const { useStatisticQuery, useStatisticsQuery } = statisticApi;
+export const {useStatisticQuery, useStatisticsQuery} = statisticApi;
