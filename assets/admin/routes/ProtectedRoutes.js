338f31fb7a46315559191ab6012c91178a51ca06
Picasso Houessou <houessoupicasso@yahoo.fr>
2024-05-25 16:29:41 +0200

    Remove unused components

--- assets/admin/routes/ProtectedRoutes.js
+++ assets/admin/routes/ProtectedRoutes.js
@@ -4,9 +4,6 @@ import Dashboard from "../dashboard/Dashboard";
 import AppCalendar from "../apps/AppCalendar";
 
 // UI Elements
-import LayoutColumns from "../docs/LayoutColumns";
-import LayoutGrid from "../docs/LayoutGrid";
-import LayoutGutters from "../docs/LayoutGutters";
 import Modules from "../pages/modules/Index";
 import Profile from "../pages/profiles/Index";
 import Logs from "@Admin/pages/Logs";
@@ -21,11 +18,7 @@ const protectedRoutes = [
     {path: `${AdminPages.MODULES}/*`, element: <Modules/>},
     {path: `${AdminPages.LOGS}/*`, element: <Logs/>},
     {path: `${AdminPages.PROFILES}/*`, element: <Profile/>},
-    {path: AdminPages.CALENDAR, element: <AppCalendar/>},
-    {path: "docs/layout/grid", element: <LayoutGrid/>},
-    {path: "docs/layout/columns", element: <LayoutColumns/>},
-    {path: "docs/layout/gutters", element: <LayoutGutters/>},
-
+    {path: AdminPages.CALENDAR, element: <AppCalendar/>}
 ]
 
 export default protectedRoutes;
