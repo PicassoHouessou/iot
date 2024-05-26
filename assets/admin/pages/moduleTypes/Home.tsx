22c6cafce2e5772105e76086fe825a89419e76cb
Picasso Houessou <houessoupicasso@yahoo.fr>
2024-05-26 19:12:46 +0200

    remove generate report button to the chart

--- assets/admin/pages/moduleTypes/Home.tsx
+++ assets/admin/pages/moduleTypes/Home.tsx
@@ -1,20 +1,23 @@
-import React, {useCallback, useEffect, useMemo, useState} from 'react';
-import {Button, Row} from 'react-bootstrap';
-import {Link} from 'react-router-dom';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { Button, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Footer from '../../layouts/Footer';
 import Header from '../../layouts/Header';
-import {useSkinMode} from '@Admin/hooks';
-import type {GetProp, MenuProps, TableProps} from 'antd';
-import {Dropdown, Table} from 'antd';
-import {useDeleteModuleMutation, useModuleTypesJsonLdQuery,} from '@Admin/services/modulesApi';
-import {ModuleType} from '@Admin/models';
-import {formatDate, getErrorMessage, useMercureSubscriber} from '@Admin/utils';
-import {AdminPages, ApiRoutesWithoutPrefix} from '@Admin/constants';
-import {toast} from 'react-toastify';
-import {useFiltersQuery, useHandleTableChange} from '@Admin/hooks/useFilterQuery';
-import {useAppSelector} from '@Admin/store/store';
-import {selectCurrentLocale} from '@Admin/features/localeSlice';
-import {useTranslation} from 'react-i18next';
+import { useSkinMode } from '@Admin/hooks';
+import type { GetProp, MenuProps, TableProps } from 'antd';
+import { Dropdown, Table } from 'antd';
+import {
+    useDeleteModuleMutation,
+    useModuleTypesJsonLdQuery,
+} from '@Admin/services/modulesApi';
+import { ModuleType } from '@Admin/models';
+import { formatDate, getErrorMessage, useMercureSubscriber } from '@Admin/utils';
+import { AdminPages, ApiRoutesWithoutPrefix } from '@Admin/constants';
+import { toast } from 'react-toastify';
+import { useFiltersQuery, useHandleTableChange } from '@Admin/hooks/useFilterQuery';
+import { useAppSelector } from '@Admin/store/store';
+import { selectCurrentLocale } from '@Admin/features/localeSlice';
+import { useTranslation } from 'react-i18next';
 
 type ColumnsType<T> = TableProps<T>['columns'];
 type TablePaginationConfig = Exclude<GetProp<TableProps, 'pagination'>, boolean>;
@@ -28,7 +31,7 @@ interface TableParams {
 
 export default function Home() {
     const currentLocale = useAppSelector(selectCurrentLocale);
-    const {t} = useTranslation();
+    const { t } = useTranslation();
     const [, setSkin] = useSkinMode();
     const [deleteItem] = useDeleteModuleMutation();
     const [data, setData] = useState<ModuleType[]>([]);
@@ -45,7 +48,7 @@ export default function Home() {
         handleSearch,
         setSearchFormValue,
     } = useFiltersQuery();
-    const {current: currentPage, itemsPerPage} = pagination;
+    const { current: currentPage, itemsPerPage } = pagination;
     const [tableParams, setTableParams] = useState<TableParams>({
         pagination: {
             current: currentPage,
@@ -61,7 +64,7 @@ export default function Home() {
         setData,
     });
 
-    const {isLoading: loading, data: dataApis} = useModuleTypesJsonLdQuery(query);
+    const { isLoading: loading, data: dataApis } = useModuleTypesJsonLdQuery(query);
 
     const handleDelete = useCallback(
         async (id: any) => {
@@ -70,7 +73,7 @@ export default function Home() {
                     await deleteItem(id).unwrap();
                     toast.success(t('Element supprimé'));
                 } catch (err) {
-                    const {detail} = getErrorMessage(err);
+                    const { detail } = getErrorMessage(err);
                     toast.error(detail);
                 }
             }
@@ -145,7 +148,7 @@ export default function Home() {
                     ];
 
                     return (
-                        <Dropdown className="" menu={{items}}>
+                        <Dropdown className="" menu={{ items }}>
                             <i className="ri-more-2-fill"></i>
                         </Dropdown>
                     );
@@ -195,7 +198,7 @@ export default function Home() {
 
     return (
         <React.Fragment>
-            <Header onSkin={setSkin}/>
+            <Header onSkin={setSkin} />
             <div className="main main-app p-3 p-lg-4">
                 <div className="d-md-flex align-items-center justify-content-between mb-4">
                     <div>
@@ -261,7 +264,7 @@ export default function Home() {
                     />
                 </Row>
 
-                <Footer/>
+                <Footer />
             </div>
         </React.Fragment>
     );
