2bcf397ba809a06c4ba17c7cecd3f2f2c8849ff6
Picasso Houessou <houessoupicasso@yahoo.fr>
2024-05-27 13:29:54 +0200

    update translation keys

--- assets/admin/pages/modules/View.tsx
+++ assets/admin/pages/modules/View.tsx
@@ -1,27 +1,26 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { Button, Col, Row, Spinner } from 'react-bootstrap';
-import { Link, useParams } from 'react-router-dom';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import {Button, Col, Row, Spinner} from 'react-bootstrap';
+import {Link, useParams} from 'react-router-dom';
 import Footer from '../../layouts/Footer';
 import Header from '../../layouts/Header';
-import { useSkinMode } from '@Admin/hooks';
-import { AdminPages } from '@Admin/constants';
-import {
-    useModuleHistoriesJsonLdQuery,
-    useModuleQuery,
-} from '@Admin/services/modulesApi';
-import { List, Tag } from 'antd';
-import { ModuleHistory } from '@Admin/models';
+import {useSkinMode} from '@Admin/hooks';
+import {AdminPages} from '@Admin/constants';
+import {useModuleHistoriesJsonLdQuery, useModuleQuery,} from '@Admin/services/modulesApi';
+import {List, Tag} from 'antd';
+import {ModuleHistory} from '@Admin/models';
+import {useTranslation} from "react-i18next";
 
 export default function View() {
-    const { id } = useParams();
+    const {id} = useParams();
+    const {t} = useTranslation();
     const loadMoreRef = useRef(null);
-    const { data: module } = useModuleQuery(id!, { skip: id ? false : true });
+    const {data: module} = useModuleQuery(id!, {skip: id ? false : true});
     const [query, setQuery] = useState<any>({
         module: id,
         'order[createdAt]': 'desc',
         itemsPerPage: 10,
     });
-    const { data: histories, isLoading } = useModuleHistoriesJsonLdQuery(query, {
+    const {data: histories, isLoading} = useModuleHistoriesJsonLdQuery(query, {
         skip: id ? false : true,
     });
     const [, setSkin] = useSkinMode();
@@ -74,7 +73,7 @@ export default function View() {
                     onLoadMore();
                 }
             },
-            { threshold: 1.0 },
+            {threshold: 1.0},
         );
 
         const currentRef = loadMoreRef.current;
@@ -98,7 +97,7 @@ export default function View() {
                 <li className="activity-date">{item?.module?.createdAt}</li>
                 <li
                     className="activity-item "
-                    style={{ backgroundColor: item?.module?.color }}
+                    style={{backgroundColor: item?.module?.color}}
                     ref={addRef ? loadMoreRef : null}
                 >
                     <p className="d-sm-flex align-items-center mb-0">
@@ -115,7 +114,7 @@ export default function View() {
 
     return (
         <React.Fragment>
-            <Header onSkin={setSkin} />
+            <Header onSkin={setSkin}/>
             <div className="main main-app p-3 p-lg-4">
                 <div className="d-md-flex align-items-center justify-content-between mb-4">
                     <div>
@@ -163,14 +162,13 @@ export default function View() {
                                 className="activity-group mb-5"
                                 loading={isLoading}
                                 itemLayout="horizontal"
-                                //loadMore={<div ref={loadMoreRef}></div>}
                                 dataSource={list}
                                 renderItem={(item, index) => renderItem(item, index)}
                                 footer={
                                     isLoading && (
                                         <Spinner animation="border" role="status">
                                             <span className="visually-hidden">
-                                                Loading...
+                                                {t("Chargement...")}
                                             </span>
                                         </Spinner>
                                     )
@@ -180,7 +178,7 @@ export default function View() {
                     </Row>
                 </div>
 
-                <Footer />
+                <Footer/>
             </div>
         </React.Fragment>
     );
