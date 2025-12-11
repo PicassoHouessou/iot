a3bc219c5e9b5dc4453fd9d1dbfdf88e1d6799b5
Picasso Houessou <houessoupicasso@yahoo.fr>
2025-12-11 12:16:21 +0100

    ignore vscode

--- assets/admin/utils/i18n.ts
+++ assets/admin/utils/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import {initReactI18next} from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
-import { availableLocales, defaultLocale } from '@Admin/constants/language';
+import {availableLocales, defaultLocale} from '@Admin/constants/language';
 
 i18n
     // i18next-http-backend
@@ -14,7 +14,7 @@ i18n
     .use(LanguageDetector)
     // pass the i18n instance to react-i18next.
     .use(initReactI18next)
-    // init i18next
+    // init i18next<
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
         detection: {
